2896559490a246fa3d68ba004012c4ed3a25b1b2
Alby Tseng <albytseng@gmail.com>
2018-03-14 20:40:11 -0700

    Update config for build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
 const _ = require('lodash');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
+const commonConfig = {
   entry: path.resolve(__dirname, 'src/index.js'),
-
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: _.camelCase('[name]'),
-    libraryTarget: 'umd',
-  },
 
   module: {
     rules: [
@@ -26,3 +20,15 @@ module.exports = {
     new CleanWebpackPlugin(['dist'], {root: path.resolve(__dirname)}),
   ],
 };
+
+module.exports = [
+  // UMD module
+  {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'limited-use.js',
+      library: _.camelCase('[name]'),
+      libraryTarget: 'umd',
+    },
+  },
+].map(outputConfig => merge(outputConfig, commonConfig));
