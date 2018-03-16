333a453e3181b03b20a9bf946ed37941f7badb72
Alby Tseng <albytseng@gmail.com>
2018-03-15 18:14:21 -0700

    Add named exports

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import LimitedUse from './limited-use';
 import CollectiveUse from './collective-use';
 import isUseful from './is-useful';
 
+export {LimitedUse, CollectiveUse, isUseful};
 export default {LimitedUse, CollectiveUse, isUseful};
